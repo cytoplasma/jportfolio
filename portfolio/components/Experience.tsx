3ca2b8d5a8fcc48bbbaa9dda00b9784fd7d7efc4
cytoplasma <jpiromg@gmail.com>
2023-02-15 21:50:12 -0800

    fixed overflow scroll for experience cards

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -14,7 +14,9 @@ export default function Experience({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[10px] text-gray-500 text-3xl font-semibold">
         Experience
       </h3>
-      <div>
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+        <ExperienceCard />
+        <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
       </div>
