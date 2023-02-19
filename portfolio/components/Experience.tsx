5db24f55001289d40e072338c70b42896cc96b52
cytoplasma <jpiromg@gmail.com>
2023-02-18 19:59:43 -0800

    scrollbar customization using tailwind

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -14,7 +14,7 @@ export default function Experience({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[10px] text-gray-500 text-3xl font-semibold">
         Experience
       </h3>
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#fff]/80">
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -6,11 +6,11 @@ import About from "../components/About";
 import Experience from "../components/Experience";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import Contact from "../components/Contact"
+import Contact from "../components/Contact";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#fff]/80">
       <Head>
         <title>Jeremiah's Portfolio</title>
       </Head>
